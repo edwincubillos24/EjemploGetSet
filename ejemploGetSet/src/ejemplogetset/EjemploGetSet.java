5b28a91d7ed8a0776a55f1e61b16ec1d4714f962
Edwin <edwinacubillos@gmail.com>
2016-08-29 18:57:12 -0500

    Ejemplo Getter, Setter y Consctructores

---
+++ ejemploGetSet/src/ejemplogetset/EjemploGetSet.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogetset;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Edwin
+ */
+public class EjemploGetSet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner lector = new Scanner (System.in);
+        Persona person = new Persona();
+        
+        System.out.println("Digite nombre: ");
+        person.setNombre(lector.next());
+        
+        System.out.println("Digite edad: ");
+        person.setEdad(lector.nextInt());
+        
+        System.out.println("El nombre es: "+person.getNombre());
+        System.out.println("La edad es: "+person.getEdad());
+    }
+    
+}
